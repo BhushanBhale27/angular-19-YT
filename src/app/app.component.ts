72651214cd6a51c6d102102e6ce005eb49c5d0bc
Bhushan Bhale <bhushan27official@gmail.com>
2025-07-21 09:09:14 +0530

    Counter application - 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports:[LoginComponent, LogoutComponent],
+  imports:[LoginComponent, LogoutComponent, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
