74602d65793af66b9fc5f096ccc6930f03881f78
Bhushan Bhale <bhushan27official@gmail.com>
2025-07-22 06:56:09 +0530

    for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,33 @@ import { CounterComponent } from './counter/counter.component';
 })
 export class AppComponent {
   title = 'angular-19-YT';
-  name = "Bhushan Bhale";
-  x = 2;
-  y = 4;
+  name:string = '';
+  email=""
+  info = ''
+  // users = ['Bhushan','Bhale','Sanu','Aai','baba'];
+  users = [
+    {name:'bhushan',age:25,color:"red"},
+    {name:'Anil',age:25,color:"red"},
+    {name:'baba',age:25,color:"red"},
+    {name:'sanu',age:25,color:"red"}
+  ]
+  getName(event:Event){
+    console.log(typeof event);
+    console.log(event);
+    if(typeof (event.target as HTMLInputElement).value === "string"){
+      this.name = ((event.target as HTMLInputElement).value);
+    } else {
+      this.name = "Enter valid name";
+    }
+  }
+  getEmail(val:string){ 
+      this.email = val;
+  }
+  setEmail(){
+    this.email = "default@gmail.com"
+  }
+
+  getNameOfObj(val:string){
+    this.info = val;
+  }
 }
